5271bcc8b540a0328574ce7708b6e0d6b2e149aa
Megan Thong <meganthong@outlook.com>
2021-03-09 07:58:43 +0800

    Update fun_1.php

--- fun_1.php
+++ fun_1.php
@@ -1,5 +1,10 @@
 <?php
 
+/* fun_1.php contains a form that accepts two integers, say n and f. 
+Edit fun_1.php such that it display swhether f is a factor of n or not. 
+You are required to implement a function called is_factor in fun_1.php that takes in 2 parameters n and f and returns True if f is a factor of n and False otherwise
+*/
+
  // add your code here
  $n = isset($_GET["n"]) ? $_GET["n"] : null;
  $f = isset($_GET["f"]) ? $_GET["f"] : null;
