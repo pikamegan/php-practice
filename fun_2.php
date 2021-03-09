e699fad8df4c20711f8714997bf07a47676e305f
Megan Thong <meganthong@outlook.com>
2021-03-09 08:00:00 +0800

    Update fun_2.php

--- fun_2.php
+++ fun_2.php
@@ -1,5 +1,10 @@
 <?php
 
+/* fun_2.php contains a form that accepts two integers, say m and n. 
+Edit fun_2.php such that it displays the sum of the powers from m^0 to m^n (i.e, m^0 + m^1 + m^2 + ... + m^n). 
+In fun_2.php, you are required to define a function named sum_of_powers that takes in 2 parameters m and n 
+and computes the sum of the powers from m^0 to m^n and returns the sum */
+
 // add your code here
 $m = isset($_GET["m"]) ? $_GET["m"] : null;
 $n = isset($_GET["n"]) ? $_GET["n"] : null;
